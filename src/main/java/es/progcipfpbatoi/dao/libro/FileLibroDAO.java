ef4e474536cc9c065c9343e35efe826451e58499
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-05-18 12:59:49 +0200

    Orden

--- src/main/java/es/progcipfpbatoi/dao/libro/FileLibroDAO.java
+++ src/main/java/es/progcipfpbatoi/dao/libro/FileLibroDAO.java
@@ -9,11 +9,11 @@ import java.util.ArrayList;
 public class FileLibroDAO implements LibroDAO {
     private static final String DATABASE_FILE     = "resources/database/libros.txt";
     private final static int    TITULO            = 0;
-    private final static int    AUTOR             = 2;
-    private final static int    FECHA_PUBLICACION = 3;
-    private final static int    NIF_EDITORIAL     = 4;
-    private final static int    TIPO              = 5;
-    private final static int    NIVEL_EDUCATIVO   = 6;
+    private final static int    AUTOR             = 1;
+    private final static int    FECHA_PUBLICACION = 2;
+    private final static int    NIF_EDITORIAL     = 3;
+    private final static int    TIPO              = 4;
+    private final static int    NIVEL_EDUCATIVO   = 5;
 
     private static final String FIELD_SEPARATOR = ";";
 
