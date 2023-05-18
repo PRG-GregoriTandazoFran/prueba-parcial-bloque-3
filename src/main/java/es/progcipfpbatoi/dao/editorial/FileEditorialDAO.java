45ab1bf88f53c8425e4c6ace7d6ab33c5f456b6c
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-05-18 12:29:50 +0200

    base realizada, falta realizar los metodos

--- src/main/java/es/progcipfpbatoi/dao/editorial/FileEditorialDAO.java
+++ src/main/java/es/progcipfpbatoi/dao/editorial/FileEditorialDAO.java
@@ -1,4 +1,40 @@
 package es.progcipfpbatoi.dao.editorial;
 
-public class FileEditorialDAO {
+import es.progcipfpbatoi.dto.Editorial;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileEditorialDAO implements EditorialDAO {
+    private static final String DATABASE_FILE = "resources/database/editorial.txt";
+    private final static int    NOMBRE        = 0;
+    private final static int    NIF           = 1;
+
+    private static final String FIELD_SEPARATOR = ";";
+
+    private File file;
+
+    public FileEditorialDAO() {
+        this.file = new File( DATABASE_FILE );
+    }
+
+    @Override
+    public void save(Editorial editorial) {
+
+    }
+
+    @Override
+    public void remove(Editorial editorial) {
+
+    }
+
+    @Override
+    public ArrayList<Editorial> findAll() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Editorial> findAllAvailables() {
+        return null;
+    }
 }
--- src/main/java/es/progcipfpbatoi/dao/libro/FileLibroDAO.java
+++ src/main/java/es/progcipfpbatoi/dao/libro/FileLibroDAO.java
@@ -1,4 +1,28 @@
 package es.progcipfpbatoi.dao.libro;
 
-public class FileLibroDAO {
+import es.progcipfpbatoi.dto.Editorial;
+import es.progcipfpbatoi.dto.Libro;
+
+import java.util.ArrayList;
+
+public class FileLibroDAO implements LibroDAO{
+    @Override
+    public void save(Libro libro) {
+
+    }
+
+    @Override
+    public void remove(Libro libro) {
+
+    }
+
+    @Override
+    public ArrayList<Editorial> findAll() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Editorial> findAllAvailables() {
+        return null;
+    }
 }
